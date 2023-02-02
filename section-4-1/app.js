85a1b8f655aed1d25374db487cfa3dd9e5f27eb2
David Ogbeide <boyowao@gmail.com>
2023-02-01 23:42:07 -0700

    experiment with different types of rendering

--- section-4-1/app.js
+++ section-4-1/app.js
@@ -4,34 +4,50 @@ let vm = Vue.createApp({
       message: "Hello world!"
     }
   },
-  beforeCreate() {
-    console.log('beforeCreate()', this.message)
-  },
-  created() {
-    console.log('created()', this.message)
-  },
-  beforeMount() {
-    console.log('beforeMount()', this.$el)
-  },
-  mounted() {
-    console.log('mounted()', this.$el)
-  },
-  beforeUpdate() {
-    console.log('beforeUpdate()')
-  },
-  updated() {
-    console.log('updated()')
-  },
-  beforeUnmount() {
-    console.log('beforeUnmount()')
-  },
-  unmounted() {
-    console.log('unmounted()')
-  }
+  template: `{{ message }}`
+  // beforeCreate() {
+  //   console.log('beforeCreate()', this.message)
+  // },
+  // created() {
+  //   console.log('created()', this.message)
+  // },
+  // beforeMount() {
+  //   console.log('beforeMount()', this.$el)
+  // },
+  // mounted() {
+  //   console.log('mounted()', this.$el)
+  // },
+  // beforeUpdate() {
+  //   console.log('beforeUpdate()')
+  // },
+  // updated() {
+  //   console.log('updated()')
+  // },
+  // beforeUnmount() {
+  //   console.log('beforeUnmount()')
+  // },
+  // unmounted() {
+  //   console.log('unmounted()')
+  // }
 })
+
 
 vm.mount('#app');
 
 // setTimeout(() => {
 //   vm.mount('#app')
 // }, 3000)
+
+let vm2 = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world!"
+    }
+  },
+  render() {
+    return Vue.h(
+      'h1',
+      this.message
+    )
+  }
+}).mount('#app2')
