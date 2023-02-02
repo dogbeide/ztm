11b4528eb59c1d96d4bfadcd2693bc650bfb84d0
David Ogbeide <boyowao@gmail.com>
2023-02-01 23:46:51 -0700

    introduce basic components

--- section-4-1/app.js
+++ section-4-1/app.js
@@ -1,36 +1,15 @@
 let vm = Vue.createApp({
-  data() {
-    return {
-      message: "Hello world!"
-    }
-  },
-  template: `{{ message }}`
-  // beforeCreate() {
-  //   console.log('beforeCreate()', this.message)
-  // },
-  // created() {
-  //   console.log('created()', this.message)
-  // },
-  // beforeMount() {
-  //   console.log('beforeMount()', this.$el)
-  // },
-  // mounted() {
-  //   console.log('mounted()', this.$el)
-  // },
-  // beforeUpdate() {
-  //   console.log('beforeUpdate()')
-  // },
-  // updated() {
-  //   console.log('updated()')
-  // },
-  // beforeUnmount() {
-  //   console.log('beforeUnmount()')
-  // },
-  // unmounted() {
-  //   console.log('unmounted()')
-  // }
+  // template: `{{ message }}`
 })
 
+vm.component('basic', {
+  template: `<h1>{{ message }}</h1>`,
+  data() {
+    return {
+      message: "Basic component, hi what's up"
+    }
+  }
+})
 
 vm.mount('#app');
 
@@ -38,16 +17,16 @@ vm.mount('#app');
 //   vm.mount('#app')
 // }, 3000)
 
-let vm2 = Vue.createApp({
-  data() {
-    return {
-      message: "Hello world!"
-    }
-  },
-  render() {
-    return Vue.h(
-      'h1',
-      this.message
-    )
-  }
-}).mount('#app2')
+// let vm2 = Vue.createApp({
+//   data() {
+//     return {
+//       message: "Hello world!"
+//     }
+//   },
+//   render() {
+//     return Vue.h(
+//       'h1',
+//       this.message
+//     )
+//   }
+// }).mount('#app2')
