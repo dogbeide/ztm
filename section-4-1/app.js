120584fbfe6225e407e4d190bfcfd4ab5b3d9182
David Ogbeide <boyowao@gmail.com>
2023-02-01 22:39:16 -0700

    use all of vue.js lifecycle methods

--- section-4-1/app.js
+++ section-4-1/app.js
@@ -3,10 +3,34 @@ let vm = Vue.createApp({
     return {
       message: "Hello world!"
     }
+  },
+  beforeCreate() {
+    console.log('beforeCreate()', this.message)
+  },
+  created() {
+    console.log('created()', this.message)
+  },
+  beforeMount() {
+    console.log('beforeMount()', this.$el)
+  },
+  mounted() {
+    console.log('mounted()', this.$el)
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()')
+  },
+  updated() {
+    console.log('updated()')
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()')
+  },
+  unmounted() {
+    console.log('unmounted()')
   }
 })
 
-vm.mount();
+vm.mount('#app');
 
 // setTimeout(() => {
 //   vm.mount('#app')
