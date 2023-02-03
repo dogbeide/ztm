562ba792599e1a1309d40ca1345121e93c1dc5af
David Ogbeide <boyowao@gmail.com>
2023-02-03 01:58:03 -0700

    add and implement slots

--- slots/src/App.vue
+++ slots/src/App.vue
@@ -1,5 +1,30 @@
 <template>
-  <app-form></app-form>
+  <app-form>
+    <template v-slot:help>
+      <p>{{ help }}</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="username">
+      <input type="text" placeholder="email">
+      <input type="password" placeholder="password">
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+    <p>fill in extra empty slot</p>
+  </app-form>
+  <app-form>
+    <template v-slot:help>
+      <p>send us a msg for feedback!</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="name">
+      <input type="text" placeholder="msg">
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+  </app-form>
 </template>
 
 <script>
@@ -9,6 +34,11 @@ export default {
   name: "App",
   components: {
     AppForm
+  },
+  data() {
+    return {
+      help: "how to fill out this form"
+    }
   }
 }
 </script>
