cf0ff67922c7062fa9a97ab9f9c6f309d2dddf88
David Ogbeide <boyowao@gmail.com>
2023-02-03 01:10:15 -0700

    rename project vue-components to components

---
+++ components/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h3>some text</h3>
+  <greeting :age="age"></greeting>
+  <user :age="age" @age-inc="updateAge" :updateAgeCB="updateAgeCB"></user>
+</template>
+
+<script>
+
+import Greeting from '@/components/Greeting.vue'
+import User from '@/components/User.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Greeting,
+    User
+  },
+  data() {
+    return {
+      age: 20
+    }
+  },
+  methods: {
+    updateAge(num) {
+      if (this.age + num >= 130) {
+        this.age = 130
+      } else {
+        this.age += num
+      }
+    },
+    updateAgeCB(num) {
+      this.updateAge(num)
+    }
+  }
+}
+  
+</script>
---
+++ components/src/components/Greeting.vue
@@ -0,0 +1,26 @@
+<template>
+  <p v-if="age > 25">{{ msg }}</p>
+  <p v-else>You must be 25 years or older to view this msg</p>
+</template>
+
+<script>
+export default {
+  name: 'Greeting',
+  props: ["age"],
+  data() {
+    return {
+      msg: "wazaaaap"
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  p {
+    color: red;
+    &:hover {
+      color: darken(#cc4444, 15%);
+    }
+  }
+
+</style>
