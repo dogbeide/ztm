72c74833b838abc4f015c6f4dc3bbe23318702c9
David Ogbeide <boyowao@gmail.com>
2023-02-03 02:30:55 -0700

    testing out dynamic components & new lifecycle hooks

---
+++ dynamic-components/src/components/About.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>msg about</p>
+</template>
---
+++ dynamic-components/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Home text</h1>
+</template>
+
+<script>
+export default {
+  unmounted() {
+    console.log('unmounted()')
+  },
+  activated() {
+    console.log('activated()')
+  },
+  deactivated() {
+    console.log('deactivated()')
+  }
+}
+</script>
