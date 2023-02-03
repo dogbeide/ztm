04303e25022b4af270b14e5f073f6eb3c33c603d
David Ogbeide <boyowao@gmail.com>
2023-02-03 13:31:56 -0700

    implement fade zoom animations

--- animations/src/App.vue
+++ animations/src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <button type="button" @click="flag = !flag">Toggle animation</button>
 
-  <transition name="fade" mode="out-in">
+  <!-- <transition name="fade" mode="out-in">
     <h2 v-if="flag" key="main">some text</h2>
     <h2 v-else key="other">Anther txt</h2>
+  </transition> -->
+
+  <transition name="zoom" type="animation" appear>
+    <h2 v-if="flag">some more text</h2>
   </transition>
   
 </template>
@@ -13,13 +17,18 @@ export default {
   name: 'App',
   data() {
     return {
-      flag: false
+      flag: true
     }
   }
 }
 </script>
 
 <style>
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
 .fade-enter-from {
   opacity: 0;
 }
@@ -29,6 +38,38 @@ export default {
 .fade-leave-to {
   opacity: 0;
   transition: all 0.08s linear;
+}
+
+.zoom-enter-from {
+  opacity: 0;
+}
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: all 0.2s linear;
+}
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: all 0.15s linear;
+}
+.zoom-leave-to {
+  opacity: 0;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0,0);
+  }
+  to {
+    transform: scale(1,1);
+  }
+}
+@keyframes zoom-out {
+  from {
+    transform: scale(1,1);
+  }
+  to {
+    transform: scale(0,0);
+  }
 }
 </style>
 
